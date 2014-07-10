6f0b3c8895f1b4071852e7d5786fd39d46a70183
Ian Barker <ianmjb@gmail.com>
2014-07-10 22:05:39 +0100

    k

--- roman_numerals.py
+++ roman_numerals.py
@@ -3,6 +3,13 @@ import Counter from collections
 
 num = "CDMXLICMLCIIVVC"
 
+multi_table = {}
+multi_table["I":{"I":"I","V":"V","X":"X","L":"L","C":"C","D":"D"}]
+multi_table["V":{"I":"V","V":"XXV","X":"L","L":"CCL","C":"D","D":"MMD"}]
+multi_table["X":{"I":"X","V":"L","X":"C","L":"D","C":"M","D":"MMMMM"}]
+multi_table["L":{"I":"L","V":"CCL","X":"D","L":"MMD","C":"MMMMM"}]
+multi_table["C":{"I":"C","V":"D","X":"M","L":"MMMMM"}]
+multi_table["D":{"I":"D","V":"MMD","X":"MMMMM"}]
 
 def sort_num(a):
 	values = {'M':1000,'D':500,'C':100,'L':50,'X':10,'V':5,'I':1}
@@ -15,3 +22,10 @@ def subtract(a, b):
 	c.subtract(Counter(b))
 	print(c)
 
+def multiply(a, b):
+	a = exploderise(a)
+	b = exploderise(b)
+	for(c1 in a):
+		for(c2 in b):
+			c.append(multitable[a][b])
+	print(c)
