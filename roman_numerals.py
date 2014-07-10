94eca56357df355b6411321cd276594e8bea13a1
Henco Appel <hencoappel@gmail.com>
2014-07-10 20:15:02 +0100

    starting subtract method and create sort function

--- roman_numerals.py
+++ roman_numerals.py
@@ -1,19 +1,17 @@
 #!/usr/bin/env python3
+import Counter from collections
 
 num = "CDMXLICMLCIIVVC"
 
-values = {
-		'M':1000,
-		'D':500,
-		'C':100,
-		'L':50,
-		'X':10,
-		'V':5,
-		'I':1
-		}
-sorter = lambda c: values[c]
 
-print("".join(sorted(num, key=sorter, reverse=True)))
+def sort_num(a):
+	values = {'M':1000,'D':500,'C':100,'L':50,'X':10,'V':5,'I':1}
+	return "".join(sorted(a, key=lambda c: values[c], reverse=True))
 
-def subtract():
-	
+def subtract(a, b):
+	a = exploderise(a)
+	b = exploderise(b)
+	c = Counter(a)
+	c.subtract(Counter(b))
+	print(c)
+
